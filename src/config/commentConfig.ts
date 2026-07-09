11dda24bdc8d9882b560b9580d1d5c34cf1b79d1
Knoon <2543327978@qq.com>
2026-07-09 10:09:15 +0800

    feat: add giscus comments section

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -3,17 +3,17 @@ import { SITE_LANG } from "./siteConfig";
 
 // 评论系统配置
 export const commentConfig: CommentConfig = {
-	enable: false, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
-	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
+	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
+	system: "giscus", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
 		envId: "", // 启用 Twikoo 前填写自己的环境 ID 或服务地址
 		lang: SITE_LANG,
 	},
 	giscus: {
-		repo: "",
-		repoId: "",
-		category: "",
-		categoryId: "",
+		repo: "Ao-chii/H2RO-Archive",
+		repoId: "R_kgDOTMgeyA",
+		category: "Announcements",
+		categoryId: "DIC_kwDOTMgeyM4DAzKT",
 		mapping: "pathname",
 		strict: "0",
 		reactionsEnabled: "1",
